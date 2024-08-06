81aedaade56b2977220ce70e563f6784cfc02bab
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-06 14:16:18 +0500

    change model.py

--- src/models.py
+++ src/models.py
@@ -24,7 +24,7 @@ intpk = Annotated[int, mapped_column(primary_key=True)]
 created_at = Annotated[datetime.datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
 updated_at = Annotated[datetime.datetime, mapped_column(
         server_default=text("TIMEZONE('utc', now())"),
-        onupdate=datetime.datetime.utcnow,
+        onupdate=datetime.datetime.now(datetime.UTC),
     )]
 
 
